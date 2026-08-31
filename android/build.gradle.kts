4252b188d220b4537cbddfc9a31258e1812916f2
Claudio Jara <holasoyarturo123.com@gmail.com>
2026-08-31 18:09:12 -0500

    Fix: Forzar compileSdk 36 en plugins con Kotlin DSL

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,10 +15,24 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library")) {
+            project.extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
+                compileSdk = 36
+                if (namespace == null) {
+                    namespace = project.group.toString()
+                }
+            }
+        }
+    }
 }
